d180d2e7c82e32f2d324965197385d57cd480dd2
Igor Kozłowski <igor.kozlowski@systell.eu>
2022-12-23 13:39:25 +0100

    Add react router and config - additional ErrorPage styles

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,17 +1,28 @@
 import {useRouteError} from "react-router";
+import styled from "@emotion/styled";
+import { Text } from '@chakra-ui/react'
 
 export const ErrorPage = () => {
     const error = useRouteError()
     console.error(error)
 
     return (
-        <div id="error-page">
-            <h1>Oopsy!</h1>
+        <ErrorPageWrapper id="error-page">
+            <Text fontSize='5xl'>Oopsy!</Text>
             <p>Sorry, an unexpected error has occurred.</p>
             <p>
                 {/* @ts-ignore */ }
                 <i>{error.statusText || error.message}</i>
             </p>
-        </div>
+        </ErrorPageWrapper>
     )
 }
+
+const ErrorPageWrapper = styled.div`
+    width: 100%;
+  min-height: 100vh;
+  display: flex; 
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`
